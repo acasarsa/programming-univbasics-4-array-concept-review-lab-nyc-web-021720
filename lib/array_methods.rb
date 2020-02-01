ce7fec8d71e305998e9e3406706b00b800b63044
Andrew J Casarsa <acasarsa@gmail.com>
2020-02-01 15:41:25 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,9 +3,9 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
-  array[max { |a, b|  }]
+  array.max { |a, b|  }
 end
 
 def find_min_value(array)
-  array[min { |a, b|  }]
+  array.min { |a, b|  }
 end
